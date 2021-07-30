81724456eb62c0084922a1e5a56c3290f42f21ba
Haneen Nasereddin <haneennasereddin1@gmail.com>
2021-07-30 18:57:29 -0500

    fixed text portion

--- script.js
+++ script.js
@@ -1,64 +1,85 @@
-//dislay the day at the top of the page (moment and changing the DOM) 
+$(document).ready(function(){
+    var plannerTimes = [
+        { hour: "9:00",
+            military: 9, 
+        },
+        {hour: "10:00",
+            military: 10,   
+        },
+        {hour: "11:00",
+            military: 11, 
+        }, 
+        {hour: "12:00",
+            military: 12,    
+        },
+        {hour: "1:00",
+            military: 13, 
+        },
+        {hour: "2:00",
+            military: 14, 
+        },
+        {hour: "3:00",
+            military: 15, 
+        },
+        {hour: "4:00",
+            military: 16, 
+        },
+        {hour: "5:00",
+            military: 17, 
+        },
+    ];
+    
 
-//function to color timeblock
-    //checks current hour vs time displayed in timblocks and colors accoringly
+    function getStorage() {
+        $("#9").val(localStorage.getItem("9"));
+        $("#10").val(localStorage.getItem("10"));
+        $("#11").val(localStorage.getItem("11"));
+        $("#12").val(localStorage.getItem("12"));
+        $("#13").val(localStorage.getItem("13"));
+        $("#14").val(localStorage.getItem("14"));
+        $("#15").val(localStorage.getItem("15"));
+        $("#16").val(localStorage.getItem("16"));
+        $("#17").val(localStorage.getItem("17"));
+            
+            }
+    date();
+    
+    function date(){
+        today = moment().format("dddd, MMMM Do YYYY");
+        $("#currentDay").text(today);
+    };
+    
+    let currentTime = moment().format("k")
+    let currentTimeAsNum = Number(currentTime);
+    
+    
+    plannerTimes.forEach(function(el){
+        let plannerRow = $("<div></div>").addClass("row justify-content-center").appendTo(".container");
+        let plannerCol1 = $("<div></div>").addClass("col-sm-2 col-md-2 col-lg-2 column1").appendTo(plannerRow).text(el.hour).attr("id", el.hour);
+        let plannerCol2 = $("<input type='text' name='textbox' maxlength='140'></input>").addClass("col-sm-9 col-md-9 col-lg-9 column2 inputTasks ").attr("id", el.military).appendTo(plannerRow)
+        $("<i><i/>").addClass("col-sm-1 col-md-1 col-lg-1 column3 saveBtn far fa-save").appendTo(plannerRow);
+        
+        
+            if (currentTimeAsNum > el.military) {
+                plannerCol2.addClass("past");
+                }
+            else if (currentTimeAsNum < el.military) {
+                plannerCol2.addClass("future");
+                }
+            else  {
+                plannerCol2.addClass("present");
+                };
+            
+                        if(el.hour === "9:00" || el.hour === "10:00" || el.hour === "11:00"){
+               plannerCol1.text(el.hour + "AM");
+                }
+            else{
+               plannerCol1.text(el.hour + "PM")
+                }
+    
+       
+    }) 
 
-//function to save entered content into local storage
-    //probably will be on form submit
-
-//function get local storage items and display them in proper timeblocks (called in init())
-
-
-
-
-$(document).ready(function() {
-
-  document.querySelector('#currentDay').textContent = moment().format('MMM DD, YYYY');
-  
-  var timeSlots = $('#time-block');
-  var timeArray = ['9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm']
-
-  function buildTimeBlocks(){
-      timeArray.forEach(function (i) {
-          var userVals = localStorage.getItem(i);
-          var rowEl = $("<div>").addClass("row");
-
-          let timeEl = $("<label>").addClass("hour col-12 col-lg-2").val(i).text(i).attr("type", "label");
-          let calInput = $("<input>").addClass("col-12 col-lg-8").attr("type", "text").val(userVals);
-          let button = $("<button>").addClass("saveBtn col-12 col-lg-2").text("save").attr("type", "button");
-
-          button.click(function (i) {
-              let userInput = calInput.val();
-              let curTime = timeEl.val();
-              localStorage.setItem(curTime, userInput);
-              calInput.text(userInput);
-          })
-
-          rowEl.append(timeEl).append(calInput).append(button);
-          timeSlots.append(rowEl);
-          colorChanger(timeEl, calInput);
-      })
-  }
-
-  function colorChanger(timeEl, calInput) {
-
-      let currentTime = moment().hours();
-      let calTime = parseInt(timeEl.val().split(" ")[0]);
-      
-      if (calTime < 6) {
-          calTime = calTime + 12;
-      }
-      
-      if (calTime < currentTime) {
-          calInput.addClass("past");
-      }
-      else if (calTime === currentTime) {
-          calInput.addClass("present");
-      }
-      else {
-          calInput.addClass("future");
-      }
-  }
-
-  buildTimeBlocks();
-})
+    
+    
+        }) 
